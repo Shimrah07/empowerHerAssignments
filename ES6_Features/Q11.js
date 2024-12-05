253f5c30ae026c744350a8a847f44dd232aa2fbb
Shimrah07 <shimrahahmedsa27@gmail.com>
2024-12-06 02:12:29 +0530

    ES Features Assignment

---
+++ ES6_Features/Q11.js
@@ -0,0 +1,24 @@
+// Q: 11
+// Title: L0 - Convert to Arrow Function with Default Parameters
+// Problem Statement: You are provided with a function multiply(a, b) 
+// that multiplies two numbers. Your task is to rewrite this function 
+// using arrow function syntax with a slight enhancement:
+//  include default parameters for both a and b. If no values are passed, a and b should default to 1.
+
+// Function Given:
+// function multiply(a , b ) {
+// return a * b; }
+
+// You need to:
+// Write the arrow function equivalent of the given function.
+// Use default values for the parameters.
+
+let multiply = (a, b) => {
+    if(a, b)
+    return a * b;
+    else
+     return -1;
+}
+
+console.log(multiply(3,4));
+console.log(multiply());
